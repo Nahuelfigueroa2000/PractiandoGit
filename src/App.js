d1897aa6e0a389222d8a711741df6830917b37a2
Nahuelfigueroa2000 <nahuel-aguanteboca@hotmail.com>
2022-08-31 00:14:44 -0300

    creando Estado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React , {useState} from 'react';
 
 function App() {
+  const [name,setName] = useState();
 
   function click(event){
     
