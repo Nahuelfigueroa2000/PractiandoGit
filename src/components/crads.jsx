56481d9eb5a15349d3b5ffb8f3e7dc831f96ab6c
Nahuelfigueroa2000 <nahuel-aguanteboca@hotmail.com>
2022-08-31 23:55:03 -0300

    finality_Age

--- src/components/crads.jsx
+++ src/components/crads.jsx
@@ -1,16 +1,27 @@
 import style from './card.module.css'
+import React , {useState} from 'react';
+
+
 
 export default function Cards({name}){
+
+    const [age,setAge] = useState(0);
+
+    function PushTheButton(event){
+        setAge(age + 1);
+        event.preventDefault();
+    }
+
     return(
+        
         <div className={style.body}>
             <h3 className={style.title}>
                 hello {name},how are you?
             </h3>
-            <button>
+            <button onClick={PushTheButton}>
                 How Old Are You
             </button>
-            
-            <HowOldAreYou age={"20"}></HowOldAreYou>
+            <HowOldAreYou age ={age}></HowOldAreYou>
         </div>
     )
 }
@@ -20,3 +31,4 @@ function HowOldAreYou(props){
         <h4>my age is, {props.age}</h4>
     )
 }
+
